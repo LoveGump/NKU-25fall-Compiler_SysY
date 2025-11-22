a56f74531c2d99d59d6034ec99cc36d377fa1281
2312236FJQ <2382175384@qq.com>
2025-11-23 01:20:45 +0800

    fix bugs

--- frontend/symbol/symbol_table.h
+++ frontend/symbol/symbol_table.h
@@ -2,7 +2,6 @@
 #define __FRONTEND_SYMBOL_SYMBOL_TABLE_H__
 
 #include <frontend/symbol/isymbol_table.h>
-#include <vector>
 #include <map>
 
 namespace FE::Sym
@@ -21,18 +20,6 @@ namespace FE::Sym
         // 当前作用域深度：-1 表示处于全局作用域；0、1、... 为局部层级 方便便利
         int curDepth_ = -1;
     public:
-        SymTable()  = default;
-        ~SymTable() = default;
-
-      private:
-        // 全局作用域符号集合（scopeLevel = -1）
-        std::map<Entry*, FE::AST::VarAttr> glbSymbols_;
-        // 局部作用域栈，每个元素为当前层的符号集合
-        std::vector<std::map<Entry*, FE::AST::VarAttr>> scopeStack_;
-        // 当前作用域深度：-1 表示处于全局作用域；0、1、... 为局部层级 方便便利
-        int curDepth_ = -1;
-
-      public:
         SymTable()  = default;
         ~SymTable() = default;
 
